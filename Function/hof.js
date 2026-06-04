9b42457d8fbbb4ce383528ae9acfbc81eb750784
itsam-13 <abhineet.am13@gmail.com>
2026-06-04 21:37:04 +0530

    add: data flow

--- Function/hof.js
+++ Function/hof.js
@@ -11,3 +11,20 @@ let twenty = discountCalculator(20);
 
 console.log(ten(200));
 console.log(twenty(240));
+
+        //             discountCalculator(10)
+        //                       │
+        //          discount = 10
+        //                       │
+        //                       ▼
+        // returns function(price){...}
+        //                       │
+        //                       ▼
+        //                  ten variable
+        //                       │
+        //              ten(200)
+        //                       │
+        //  price = 200, discount = 10
+        //                       │
+        //                       ▼
+        //              returns 180
