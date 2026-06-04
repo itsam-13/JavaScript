3480a9812ac92c31e88758da94451299594c382c
itsam-13 <abhineet.am13@gmail.com>
2026-06-04 21:02:49 +0530

    add: discount calculator using higher order function

---
+++ Function/hof.js
@@ -0,0 +1,13 @@
+//discount calculator using higher order function
+
+function discountCalculator(discount){
+    return function(price){
+        return price - price*(discount/100)
+    }
+}
+
+let ten = discountCalculator(10); //ten is the new function 
+let twenty = discountCalculator(20);
+
+console.log(ten(200));
+console.log(twenty(240));
