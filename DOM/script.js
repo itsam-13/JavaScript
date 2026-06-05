8207b9f7a9a9b5e07432f27c306a005758951493
itsam-13 <abhineet.am13@gmail.com>
2026-06-05 16:39:25 +0530

    add: Attribute Manipulation

--- DOM/script.js
+++ DOM/script.js
@@ -4,3 +4,14 @@ hello.textContent = "Hello kese hai aap log"
 
 hello.innerHTML = "<i>Hello</i>" //hello kese ho is replaced by Hello with italic
 
+let a = document.querySelector("a")
+console.dir(a)
+// a.href = "https://www.google.com"
+a.setAttribute("href", "https://www.google.com")
+
+let img = document.querySelector("img")
+img.setAttribute("alt", "yohohohohho")
+img.setAttribute("src", "https://images.unsplash.com/photo-1779952274981-ea7074647d8b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8")
+
+console.log(img.getAttribute("src"));
+img.removeAttribute("alt")
