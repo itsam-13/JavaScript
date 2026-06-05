f3ca4e052511ed80fddc3bf8dc8264f0b92f66a3
itsam-13 <abhineet.am13@gmail.com>
2026-06-05 16:56:38 +0530

    add: dynamic dom manipulation (createElement)

--- DOM/script.js
+++ DOM/script.js
@@ -11,7 +11,15 @@ a.setAttribute("href", "https://www.google.com")
 
 let img = document.querySelector("img")
 img.setAttribute("alt", "yohohohohho")
-img.setAttribute("src", "https://images.unsplash.com/photo-1779952274981-ea7074647d8b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8")
+// img.setAttribute("src", "https://images.unsplash.com/photo-1779952274981-ea7074647d8b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8")
 
 console.log(img.getAttribute("src"));
 img.removeAttribute("alt")
+
+
+
+
+let hey = document.createElement("h2") //blank h2 
+hey.textContent = "this is Heading 2"
+document.body.append(hey)
+
