482c23860126545feccf1b4b78e6797ac5cae94e
itsam-13 <abhineet.am13@gmail.com>
2026-06-05 16:19:28 +0530

    add: DOM folder with how to select elements in HTML

---
+++ DOM/script.js
@@ -0,0 +1,6 @@
+let hello = document.querySelector("h1")
+console.dir(hello)
+hello.textContent = "Hello kese hai aap log"
+
+hello.innerHTML = "<i>Hello</i>" //hello kese ho is replaced by Hello with italic
+
