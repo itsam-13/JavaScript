dba2d4f15a708d96fb19c0d6f1931e9f2ee0674f
itsam-13 <abhineet.am13@gmail.com>
2026-06-05 17:13:50 +0530

    add: style update using DOM

--- DOM/script.js
+++ DOM/script.js
@@ -4,6 +4,8 @@ hello.textContent = "Hello kese hai aap log"
 
 hello.innerHTML = "<i>Hello</i>" //hello kese ho is replaced by Hello with italic
 
+
+
 let a = document.querySelector("a")
 console.dir(a)
 // a.href = "https://www.google.com"
@@ -11,6 +13,7 @@ a.setAttribute("href", "https://www.google.com")
 
 let img = document.querySelector("img")
 img.setAttribute("alt", "yohohohohho")
+
 // img.setAttribute("src", "https://images.unsplash.com/photo-1779952274981-ea7074647d8b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8")
 
 console.log(img.getAttribute("src"));
@@ -23,3 +26,13 @@ let hey = document.createElement("h2") //blank h2
 hey.textContent = "this is Heading 2"
 document.body.append(hey)
 
+
+
+
+let styleH1 = document.querySelector("h1")
+// styleH1.style.color = "#ff00ff"
+// styleH1.style.backgroundColor = "cyan"
+// styleH1.style.fontFamily = "Gilroy"
+
+styleH1.classList.add("pookie") //recommended way!!
+styleH1.classList.toggle("pookie") //recommended way!!
